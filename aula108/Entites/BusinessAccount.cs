0634b8396ea023f6141ce070a46fac6febbc3cd9
GRUPPY02 <GRUPPY02@DESKTOP-B3K8HS6>
2025-03-26 17:51:48 -0300

    Adicionar arquivos de projeto.

---
+++ aula108/Entites/BusinessAccount.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace aula108.Entites
+{
+    class BusinessAccount : Account
+    {
+        public double LoanLimit { get; set; }
+
+        public BusinessAccount(int number, string holder, double balance, double LoanLimit) : base(number, holder, balance)
+        {
+            LoanLimit = LoanLimit;
+        }
+
+        public void Loan(double amount)
+        {
+            if (amount <= LoanLimit)
+            {
+                Balance += amount;
+            }
+        }
+
+    }
+}
